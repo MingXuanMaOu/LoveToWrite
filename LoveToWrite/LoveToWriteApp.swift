0a14d325e8cf4eb691cd59442e39d6269efe74e0
MingXuMaOu <92174693+MingXuMaOu@users.noreply.github.com>
2023-01-12 00:05:03 +0800

    Initial Commit

---
+++ LoveToWrite/LoveToWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveToWriteApp.swift
+//  LoveToWrite
+//
+//  Created by 刘铭 on 2023/1/12.
+//
+
+import SwiftUI
+
+@main
+struct LoveToWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
